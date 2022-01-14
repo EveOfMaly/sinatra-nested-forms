14ae7603ef60439110c47dd3c878919c7636ff65
EveOfMaly <john.a.kyeremeh@gmail.com>
2022-01-14 18:54:07 -0500

    commit tree 1e735aff1594c71d2cae990ba656216d22fe95db

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,21 @@
 class Pirate
+    attr_accessor :name, :weight, :height
+
+    @@all = []
+
+    def initialize(parameters)
+        @name = parameters[:name]
+        @weight = parameters[:weight]
+        @height = parameters[:height]
+        self.class.all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.clear 
+        @@all.clear 
+    end
+
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,21 @@
 class Ship
+    attr_accessor :name, :type, :booty
+
+    @@all  = []
+
+    def initialize(parameters)
+        @name = parameters[:name]
+        @type = parameters[:type]
+        @booty = parameters[:booty]
+        self.class.all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.clear 
+        @@all.clear 
+    end
+    
 end
